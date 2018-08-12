92cd4254bc8d7a16565f33697edcce7ebeb94623
Tyler Matheson <t_matheson@hotmail.com>
2018-08-12 22:27:28 +1000

    updated pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,12 +1,18 @@
-def project = 'tyler-matheson/argus-aws'
-def url = new URL("https://api.github.com/repos/${project}/contents/builds")
-def builds = new groovy.json.JsonSlurper().parse(url.newReader())
-builds.each {
+def account = 'tyler-matheson'
+def url = new URL("https://api.github.com/users/${account}/repos")
+def repos = new groovy.json.JsonSlurper().parse(url.newReader())
+repos.each {
     def buildName = it.name
-    def jobName = "${project}-${buildName}".replaceAll('/','-')
-    job(jobName) {
-        scm {
-            git("git://github.com/${project}/builds/${buildName}.git")
+    def jobName = "${account}.${buildName}".replaceAll('/','-')
+
+    def url = new URL("https://api.github.com/repos/${account}/${buildName}/contents/")
+    def content = new groovy.json.JsonSlurper().parse(url.newReader())
+    content.each {
+        if (it.name == "Jenkinsfile")
+        job(jobName) {
+            scm {
+                git("git://github.com/${account}/${buildName}.git")
+            }
         }
     }
 }

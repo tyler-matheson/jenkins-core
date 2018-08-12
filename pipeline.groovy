9b3c304898272f1b9e04ad812b7fd2e5e8770332
Tyler Matheson <t_matheson@hotmail.com>
2018-08-12 23:08:02 +1000

    updated pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,15 +3,17 @@ def url = new URL("https://api.github.com/users/${account}/repos")
 def repos = new groovy.json.JsonSlurper().parse(url.newReader())
 repos.each {
     def buildName = it.name
-    def jobName = "${account}.${buildName}".replaceAll('/','-')
+    def jobName = "${account}_${buildName}".replaceAll('/','-')
 
     def url2 = new URL("https://api.github.com/repos/${account}/${buildName}/contents/")
     def content = new groovy.json.JsonSlurper().parse(url2.newReader())
     content.each {
         if (it.name == "Jenkinsfile")
-        job(jobName) {
-            scm {
-                git("git://github.com/${account}/${buildName}.git")
+        multibranchPipelineJob('jobName') {
+            branchSources {
+                git {
+                    remote("https://github.com/${account}/${buildName}.git")
+                }
             }
         }
     }

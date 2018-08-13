06e8d9fd41732df97c422a5cb3647224017c50ac
Tyler Matheson <t_matheson@hotmail.com>
2018-08-13 10:30:46 +1000

    pipeline.groovy restructure

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,21 +1,17 @@
 def project = 'tyler-matheson/builds'
-def branchApi = new URL("https://api.github.com/repos/${project}/branches")
-def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
-branches.each {
-    def branchName = it.name
-    def jobName = "${project}_${branchName}".replaceAll('/','_')
+def jobName = "${project}".replaceAll('/','_')
 
-    def url2 = new URL("https://api.github.com/repos/${project}/contents/")
-    def content = new groovy.json.JsonSlurper().parse(url2.newReader())
-    content.each {
-        if (it.name == "Jenkinsfile"){
-            multibranchPipelineJob("${jobName}") {
-                branchSources {
-                    git {
-                        git("git://github.com/${project}.git", branchName)
-                    }
-                }
-            }
+multibranchPipelineJob("${jobName}") {
+    branchSources {
+        git {
+            remote "git://github.com/${project}.git"
         }
+    }
+    configure { node ->
+        (node / 'factory').@class = 'org.jenkinsci.plugins.pipeline.multibranch.defaults.PipelineBranchDefaultsProjectFactory'
+        (node / 'factory').@plugin = 'pipeline-multibranch-defaults'
+
+        def traits = node / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
     }
 }

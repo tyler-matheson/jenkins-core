e89d5cde8e6705579b9d24ca0dec3708af1aa22e
tyler-matheson <t_matheson@hotmail.com>
2018-10-03 13:11:51 +1000

    work on scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-CREDENTIAL=$(aws sts assume-role --role-arn arn:aws:iam::115266808660:role/BuildRole --role-session-name BuildRole --duration 900 --output text --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken,Expiration]")
+CREDENTIAL=$(aws sts assume-role \
+--role-arn arn:aws:iam::115266808660:role/BuildRole \
+--role-session-name BuildRole \
+--duration 900 \
+--output text \
+--query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken,Expiration]")
 
 export AWS_ACCESS_KEY_ID=$(echo $CREDENTIAL | cut -d ' ' -f 1)
 export AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIAL | cut -d ' ' -f 2)
 export AWS_SESSION_TOKEN=$(echo $CREDENTIAL | cut -d ' ' -f 3)
+export AWS_DEFAULT_REGION=us-east-1
 
-LAUNCH=$(aws ec2 run-instances --image-id ami-ede6318f /
-    --count 1 /
-    --instance-type f1.2xlarge /
-    --key-name fpgadevelopment /
-    --security-groups sg-31cb4249 /
-    --instance-initiated-shutdown-behavior terminate /
-    --user-data file://launch.sh)
+LAUNCH=$(aws ec2 run-instances --image-id ami-ede6318f \
+--count 1 \
+--instance-type f1.2xlarge \
+--key-name fpga-dev-us-1 \
+--security-groups sg-31cb4249 \
+--instance-initiated-shutdown-behavior terminate \
+--user-data file://launch.sh)
 
 INSTANCE=$(echo "$LAUNCH" | awk '/^INSTANCE/ {print $2}')
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,15 +5,16 @@ yes | sudo yum install awslogs
 
 sudo service awslogs start
 
+project_name=cl_hello_world
+
 git clone https://github.com/aws/aws-fpga.git $AWS_FPGA_REPO_DIR  
-git clone https://github.com/tyler-matheson/jenkins-core.git $CODE_CHANGES
+#git clone https://github.com/tyler-matheson/jenkins-core.git $CODE_CHANGES
 
 cd $AWS_FPGA_REPO_DIR  
 
 source hdk_setup.sh
-source sdk_setup.sh
 
-cd $HDK_DIR/cl/examples/cl_hello_world
+cd $HDK_DIR/cl/examples/$project_name
 export CL_DIR=$(pwd)
 
 cd $CL_DIR/build/scripts
@@ -22,17 +23,26 @@ export EMAIL="t_matheson@hotmail.com"
 $HDK_COMMON_DIR/scripts/notify_via_sns.py
 ./aws_build_dcp_from_cl.sh -notify -foreground
 
-aws s3 mb s3://tyler-aws-us/example/
-aws s3 rm s3://tyler-aws-us/example/*.Developer_CL.tar
-aws s3 cp $CL_DIR/build/checkpoints/to_aws/*.Developer_CL.tar s3://tyler-aws-us/example/
+path="$(find ../checkpoints/to_aws/ -regex ".*\.Developer_CL.tar$")"
+checkpoint=$(basename "$path")
 
-aws s3 mb s3://tyler-aws-us/logs/
+aws s3 mb s3://tyler-projects/$project_name/
+aws s3 rm s3://tyler-projects/$project_name/*.Developer_CL.tar
+aws s3 cp $CL_DIR/build/checkpoints/to_aws/$checkpoint s3://tyler-aws-us/$project_name/
+
 touch LOGS_FILES_GO_HERE.txt
-aws s3 cp LOGS_FILES_GO_HERE.txt s3://tyler-aws-us/logs/
+aws s3 cp LOGS_FILES_GO_HERE.txt s3://tyler-projects/$project_name/logs/
 
 #Collect AFI id and global AFI id
-aws ec2 create-fpga-image --name <afi-name> --description <afi-description> --input-storage-location Bucket=<dcp-bucket-name>,Key=<path-to-tarball> --logs-storage-location Bucket=<logs-bucket-name>,Key=<path-to-logs>      
+afi=$(aws ec2 create-fpga-image --name $project_name \
+    --input-storage-location Bucket=tyler-projects,Key=$project_name/$checkpoint \
+    --logs-storage-location Bucket=tyler-projects,Key=$project_name/logs/)
 
-$HDK_COMMON_DIR/scripts/wait_for_afi.py --afi #afi-id
+echo afi >> afi_ids.txt
+aws s3 cp afi_ids.txt s3://tyler-aws-us/$project_name/
 
-shutdown -h now
+#$HDK_COMMON_DIR/scripts/wait_for_afi.py --afi #afi-id
+
+#shutdown -h now
+
+#aws ec2 describe-fpga-images --filters Name=name,Values=cl_ddr
